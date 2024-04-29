5ba2b9913732c13ce0ea662e5a00d58716d3de44
zuhaib43423 <155728849+zuhaib43423@users.noreply.github.com>
2024-04-29 12:43:28 +0500

    Update sidebar.js

--- scorexec/src/components/common/custom-sidebar/sidebar.js
+++ scorexec/src/components/common/custom-sidebar/sidebar.js
@@ -1,11 +1,6 @@
-<<<<<<< Updated upstream:scorexec/src/pages/Sidbar/sidebar.js
-import React, { useState } from 'react';
-import { Layout, Menu, Button, Drawer, Switch, ConfigProvider } from 'antd';
-=======
 import React, { useState } from "react";
 import { Layout, Menu, ConfigProvider } from "antd";
 import { Link } from "react-router-dom";
->>>>>>> Stashed changes:scorexec/src/components/common/custom-sidebar/sidebar.js
 import {
   ProjectOutlined,
   UserOutlined,
@@ -16,19 +11,14 @@ import {
   MenuOutlined,
   CloseCircleOutlined,
   BulbOutlined,
-<<<<<<< Updated upstream:scorexec/src/pages/Sidbar/sidebar.js
-} from '@ant-design/icons';
-import 'antd/dist/reset.css';
-=======
 } from "@ant-design/icons";
 import "antd/dist/reset.css";
 import "./sidebar.css";
 import CustomButton from "../custom-button";
->>>>>>> Stashed changes:scorexec/src/components/common/custom-sidebar/sidebar.js
 
 const { Sider } = Layout;
 
-const Sidebar = () => {
+const Sidebar = ({ collapsed = false }) => {
   const [visible, setVisible] = useState(false);
   const [darkMode, setDarkMode] = useState(false);
 
@@ -45,25 +35,18 @@ const Sidebar = () => {
   };
 
   return (
-<<<<<<< Updated upstream:scorexec/src/pages/Sidbar/sidebar.js
-    <ConfigProvider theme={darkMode ? 'dark' : 'light'}>
-      <div>
-        <Button 
-          onClick={showDrawer} 
-          type="primary" 
-          icon={<MenuOutlined />} 
-          style={{ position: 'absolute', top: 20, left: 20 }}
-=======
     <ConfigProvider theme={darkMode ? "dark" : "light"}>
       <Sider
         trigger={null}
         collapsible
         collapsed={collapsed}
-        style={{ overflow: 'auto',
-        height: '100vh',
-        left: 0,
-        top: 0,
-        bottom: 0, }}
+        style={{
+          overflow: "auto",
+          height: "100vh",
+          left: 0,
+          top: 0,
+          bottom: 0,
+        }}
         title="Score XEC"
         bodyStyle={{ paddingBottom: 80 }}
         footer={
@@ -127,82 +110,6 @@ const Sidebar = () => {
           </Menu.Item>
         </Menu>
       </Sider>
-      {/* <Drawer
-        width={"200px"}
-        title="Score XEC"
-        placement="left"
-        closable={false}
-        onClose={onClose}
-        visible={visible}
-        bodyStyle={{ paddingBottom: 80 }}
-        footer={
-          <div style={{ textAlign: "right" }}>
-            <Button onClick={onClose} type="primary">
-              {<CloseCircleOutlined />}
-            </Button>
-          </div>
-        }
-      >
-        <Menu
-          mode="inline"
-          defaultSelectedKeys={["1"]}
-          theme={darkMode ? "dark" : "light"}
->>>>>>> Stashed changes:scorexec/src/components/common/custom-sidebar/sidebar.js
-        >
-          Click Me 
-        </Button>
-        <Drawer
-          title="Score XEC"
-          placement="left"
-          closable={false}
-          onClose={onClose}
-          visible={visible}
-          bodyStyle={{ paddingBottom: 80 }}
-          footer={
-            <div style={{ textAlign: "right" }}>
-              <Button onClick={onClose} type="primary">
-                {<CloseCircleOutlined />}
-              </Button>
-            </div>
-          }
-        >
-          <Sider>
-            <Menu mode="inline" defaultSelectedKeys={['1']}>
-              <Menu.Item key="1" icon={<ProjectOutlined />}>
-                Project Name
-              </Menu.Item>
-              <Menu.Item key="2" icon={<UserOutlined />}>
-                Admin
-              </Menu.Item>
-              <Menu.Item key="3" icon={<HomeOutlined />}>
-                Home Page
-              </Menu.Item>
-              <Menu.Item key="4" icon={<TeamOutlined />}>
-                Tenants
-              </Menu.Item>
-              <Menu.Item key="5" icon={<UserOutlined />}>
-                Users
-              </Menu.Item>
-              <Menu.Item key="6" icon={<CrownOutlined />}>
-                Roles
-              </Menu.Item>
-              <Menu.Item key="7" icon={<InfoCircleOutlined />}>
-                About
-              </Menu.Item>
-              <Menu.SubMenu key="sub1" title="Multi Level Menu" icon={<MenuOutlined />}>
-                <Menu.Item key="8">Option 1</Menu.Item>
-                <Menu.Item key="9">Option 2</Menu.Item>
-              </Menu.SubMenu>
-            </Menu>
-          </Sider>
-          <div style={{ padding: '20px', textAlign: 'left' }}>
-            <span style={{ marginRight: '5px' }}>
-              <BulbOutlined /> Dark Mode:
-            </span>
-            <Switch checked={darkMode} onChange={toggleDarkMode} />
-          </div>
-        </Drawer>
-      </div>
     </ConfigProvider>
   );
 };
